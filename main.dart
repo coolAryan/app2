74679c191e75f33a4bf7fb6c7a84103539968362
coolAryan <42404394+coolAryan@users.noreply.github.com>
2020-07-28 13:08:35 +0530

    Main

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'ui/home.dart';
+//import 'ui/profile.dart';
+
+main() {
+  runApp(Shot());
+}
+
+class Shot extends StatelessWidget {
+  build(BuildContext context) {
+    return myapp();
+  }
+}
